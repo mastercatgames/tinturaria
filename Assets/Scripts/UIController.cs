f8791fdd97fc85561929868f9a4921d956e0c70e
martahil <marta.h.88@hotmail.com>
2020-06-12 14:10:45 -0300

    Panels modifications (open and close)

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject inkPanel;
+    public GameObject formsPanel;
+    public GameObject ButtonsGrid;
+
+    public void OpenPanel(string panel)
+    {        
+        if (panel == "ink")
+        {            
+            inkPanel.SetActive(true);                       
+        }
+
+        if (panel == "form")
+        {            
+            formsPanel.SetActive(true);                       
+        }
+
+        ButtonsGrid.SetActive(false);
+    }
+
+    public void ClosePanel(string panel)
+    {        
+        if (panel == "ink")
+        {            
+            inkPanel.SetActive(false);                       
+        }
+
+        if (panel == "form")
+        {            
+            formsPanel.SetActive(false);                       
+        }
+
+        ButtonsGrid.SetActive(true);
+    }
+}
