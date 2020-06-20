75ace9784b9f9f8c5e881e2a82ed1bba30247e84
Augusto Polonio <augustodrp@gmail.com>
2020-06-20 18:48:58 -0300

    reopen #15 Box Request - beginning of dev

---
+++ Assets/Scripts/RequestBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RequestBox : MonoBehaviour
+{
+    private Image bar;
+    public bool timeIsOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = transform.Find("BarBG").Find("Bar").GetComponent<Image>();
+
+        //InvokeRepeating("CountdownBar", 3f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!timeIsOver && bar.fillAmount > 0.009f)
+            bar.fillAmount = Mathf.Lerp(bar.fillAmount, 0f, Time.deltaTime * 0.5f);
+        else {
+            timeIsOver = true;
+            Debug.Log("TimeIsOver!");
+        }
+    }
+
+    // void CountdownBar()
+    // {
+    //     if (!timeIsOver && bar.fillAmount > 0.009f)
+    //         bar.fillAmount = Mathf.Lerp(bar.fillAmount, 0f, Time.deltaTime * 2f);
+    //     else
+    //     {
+    //         timeIsOver = true;
+    //         Debug.Log("TimeIsOver!");
+    //     }
+    // }
+
+    void ClearUpBox()
+    {
+
+    }
+}
