d7f7e8c402763e44dea2218a94885cae1c4942ee
Augusto Polonio <augustodrp@gmail.com>
2020-06-20 23:20:47 -0300

    reopen #Queue of requests (like in Overcooked 2) - Different forms and colors; animation when failure; loading bar changing colors depending of percentage

--- Assets/Scripts/RequestBox.cs
+++ Assets/Scripts/RequestBox.cs
@@ -6,39 +6,60 @@ using UnityEngine.UI;
 public class RequestBox : MonoBehaviour
 {
     private Image bar;
-    public bool timeIsOver;
+    private RequestPanelController requestPanelController;
+    public bool isDestroyingBox;
+    private Image maskColor;
+    private float barSpeed;    
+    public Color colorRequested;
+    public Sprite boxRequested;
     // Start is called before the first frame update
     void Start()
     {
         bar = transform.Find("BarBG").Find("Bar").GetComponent<Image>();
-
-        //InvokeRepeating("CountdownBar", 3f, 1f);
+        maskColor = transform.Find("MaskColor").GetComponent<Image>();
+        requestPanelController = transform.parent.GetComponent<RequestPanelController>();
+        //TODO: Test the best time to fit in gameplay
+        barSpeed = 0.02f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!timeIsOver && bar.fillAmount > 0.009f)
-            bar.fillAmount = Mathf.Lerp(bar.fillAmount, 0f, Time.deltaTime * 0.5f);
-        else {
-            timeIsOver = true;
-            Debug.Log("TimeIsOver!");
+        if (bar.fillAmount > 0.009f)
+        {
+            bar.fillAmount -= Time.deltaTime * barSpeed;//Mathf.Lerp(bar.fillAmount, 0f, Time.deltaTime * barSpeed);
+
+            if (bar.fillAmount < 0.5f)
+            {
+                bar.color = requestPanelController.YellowColor;
+            }
+            
+            if (bar.fillAmount < 0.2f)
+            {
+                bar.color = requestPanelController.RedColor;
+            }
+        }
+        else
+        {
+            //Execute once time
+            if (!isDestroyingBox)
+            {
+                isDestroyingBox = true;
+                maskColor.color = requestPanelController.RedColor;
+                var tempColor = maskColor.color;
+                tempColor.a = 0.5f;
+                maskColor.color = tempColor;
+
+                GetComponent<Animator>().Play("deliveryFailed");
+            }
         }
     }
 
-    // void CountdownBar()
-    // {
-    //     if (!timeIsOver && bar.fillAmount > 0.009f)
-    //         bar.fillAmount = Mathf.Lerp(bar.fillAmount, 0f, Time.deltaTime * 2f);
-    //     else
-    //     {
-    //         timeIsOver = true;
-    //         Debug.Log("TimeIsOver!");
-    //     }
-    // }
-
-    void ClearUpBox()
+    void DestroyBox()
     {
-
+        //TODO: Discount from a new variable numberOfFailedDeliveries (in GameController) 
+        //TODO: Discount coins
+        transform.parent.GetComponent<RequestPanelController>().numOfRequests--;
+        Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/RequestPanelController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RequestPanelController : MonoBehaviour
+{
+    public GameObject requestBoxPrefab;
+    public int numOfRequests;
+    public Color RedColor;
+    public Color GreenColor;
+    public Color YellowColor;
+    public GameObject[] boxes;
+    public GameObject[] colors;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RequestBox", 1f, 15f);
+    }
+
+    void RequestBox()
+    {
+        if (numOfRequests < 3)
+        {
+            GameObject box = Instantiate(requestBoxPrefab);
+
+            box.GetComponent<RequestBox>().boxRequested = boxes[Random.Range(0,6)].transform.Find("Box").GetComponent<SpriteRenderer>().sprite;
+            box.GetComponent<RequestBox>().colorRequested = colors[Random.Range(0,6)].transform.Find("Ink").GetComponent<SpriteRenderer>().color;
+
+            box.transform.Find("Form").GetComponent<Image>().sprite = box.GetComponent<RequestBox>().boxRequested;
+            box.transform.Find("Color").GetComponent<Image>().color = box.GetComponent<RequestBox>().colorRequested;
+            
+            //Instantiate the requested box inside the RequestPanel
+            box.transform.SetParent(transform);
+            numOfRequests++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
