d7997d8f106f674aecdcf29b48da99e332015213
Augusto Polonio <augustodrp@gmail.com>
2020-06-21 23:14:18 -0300

    reopen #15 Box Delivery

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,28 +11,29 @@ public class GameController : MonoBehaviour
     public bool isPainting;
     public GameObject currentRepository;
     public GameObject currentBox;
-    public AudioClip[] liquidClips;    
+    public AudioClip[] liquidClips;
     public AudioSource InkMachine_AS;
     public bool isChangingRepository;
     private UIController uiController;
     public GameObject PanelForms;
-    public float[] repositoryXPositions = { -160f, -80f, 0f, 80f, 160f, 240f};
+    public GameObject RequestPanel;
+    public float[] repositoryXPositions = { -160f, -80f, 0f, 80f, 160f, 240f };
 
     //public GameObject InkShopButton; // *** M ***   
 
     void Start()
-    {        
+    {
         //Initialize values
         paintSpeed = 1.5f;
         uiController = GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>();
 
         //Load repository position randomically
         ShuffleArray(repositoryXPositions);
-        
+
         GameObject[] repositories = GameObject.FindGameObjectsWithTag("Repository");
         for (int i = 0; i < repositories.Length; i++)
         {
-            repositories[i].transform.localPosition = new Vector3(repositoryXPositions[i] , repositories[i].transform.localPosition.y, repositories[i].transform.localPosition.z);
+            repositories[i].transform.localPosition = new Vector3(repositoryXPositions[i], repositories[i].transform.localPosition.y, repositories[i].transform.localPosition.z);
         }
     }
 
@@ -52,21 +53,15 @@ public class GameController : MonoBehaviour
 
     public void NewPaintFluid()
     {
-        if (!isChangingRepository 
-        && !isPainting 
+        if (!isChangingRepository
+        && !isPainting
         && !currentRepository.GetComponent<InkRepositoryController>().isFilling
         && currentBox)
         {
             if (currentBox.GetComponent<BoxController>().currentColor != currentRepository.transform.Find("Ink").GetComponent<SpriteRenderer>().color
              && currentRepository.GetComponent<InkRepositoryController>().inkfillAmount > 0f)
             {
-                //Destroy all metaballs inside the box (because the color was changed)
-                //Destroy only clones, keeping the original
-                foreach (Transform item in currentBox.transform.Find("InsideBox").transform)
-                {
-                    if (item.name.Contains("Clone"))
-                        GameObject.Destroy(item.gameObject);
-                }
+                DestroyAllMetaballs();
 
                 //Reset percentage
                 currentBox.GetComponent<BoxController>().percentage = 0f;
@@ -92,10 +87,44 @@ public class GameController : MonoBehaviour
         }
         else
         {
-            isPainting = false;
+
             currentRepository.GetComponent<InkRepositoryController>().limitToFill -= 0.25f;
-            currentBox.GetComponent<BoxController>().percentage += 0.25f;
             currentBox.GetComponent<BoxController>().currentColor = Water2D.Water2D_Spawner.instance.WaterMaterial.color;
+            currentBox.GetComponent<BoxController>().percentage += 0.25f;
+
+            if (currentBox.GetComponent<BoxController>().percentage == 1)
+            {
+                //Verify if exists this box request
+                Transform requestedBox = null;
+
+                foreach (Transform child in RequestPanel.transform)
+                {
+                    Debug.Log("Form: " + child.Find("Form").GetComponent<Image>().sprite.name + "/ Color: " + child.Find("Color").GetComponent<Image>().color);
+
+                    if (child.Find("Form").GetComponent<Image>().sprite.name == currentBox.transform.Find("Box").GetComponent<SpriteRenderer>().sprite.name
+                    && child.Find("Color").GetComponent<Image>().color == currentBox.GetComponent<BoxController>().currentColor)
+                    {
+                        requestedBox = child;
+                    }
+                }
+
+                if (requestedBox)
+                {
+                    Debug.Log("Delivered!");
+                    requestedBox.GetComponent<RequestBox>().DeliveryBox();
+                }
+                else
+                {
+                    Debug.Log("Doesn't Match!");
+                }                
+
+                DestroyAllMetaballs();
+                currentBox.GetComponent<BoxController>().percentage = 0;
+                currentBox.SetActive(false);
+                currentBox = null;
+            }
+
+            isPainting = false;            
         }
     }
 
@@ -111,12 +140,25 @@ public class GameController : MonoBehaviour
 
         Water2D.Water2D_Spawner.instance.SetWaterColor(newBox.GetComponent<BoxController>().currentColor, Color.Lerp(newBox.GetComponent<BoxController>().currentColor, Color.white, .2f));
         newBox.SetActive(true);
-        currentBox = newBox;    
-        uiController.ClosePanel(PanelForms);  
+        currentBox = newBox;
+        uiController.ClosePanel(PanelForms);
     }
 
-    void ShuffleArray<T>(T[] arr) {
-        for (int i = arr.Length - 1; i > 0; i--) {
+    void DestroyAllMetaballs()
+    {
+        //Destroy all metaballs inside the box (because the color was changed)
+        //Destroy only clones, keeping the original
+        foreach (Transform item in currentBox.transform.Find("InsideBox").transform)
+        {
+            if (item.name.Contains("Clone"))
+                GameObject.Destroy(item.gameObject);
+        }
+    }
+
+    void ShuffleArray<T>(T[] arr)
+    {
+        for (int i = arr.Length - 1; i > 0; i--)
+        {
             int r = Random.Range(0, i);
             T tmp = arr[i];
             arr[i] = arr[r];
