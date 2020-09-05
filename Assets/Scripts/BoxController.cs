11395a7290e5e7e3bc47c9b985142e4b0c2f7f6b
Augusto Polonio <augustodrp@gmail.com>
2020-09-05 03:31:56 -0300

    reopen #28 Animation to drop box when change it

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -6,4 +6,9 @@ public class BoxController : MonoBehaviour
 {
     public Color currentColor;
     [Range (0f,1f)]public float percentage = 0; //only to know how much is inside box
+
+    public void PlayDropBoxSFX()
+    {
+        gameObject.transform.parent.GetComponent<AudioSource>().Play();
+    }
 }
