85a9f3a3275dc9c19c04fe1982bf71d1552f372c
martahil <marta.h.88@hotmail.com>
2020-09-28 22:44:14 -0300

    #15 reopen (stars x coins challenge was changed)

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -55,114 +55,114 @@ public class LevelManager : MonoBehaviour
             uiController.timeRemaining = 120f;
             SetBrokenBottlesPosition(1);
 
-            oneStarCoins = 100;
-            twoStarCoins = 200;
-            threeStarCoins = 300;
+            oneStarCoins = 200;
+            twoStarCoins = 300;
+            threeStarCoins = 400;
         }
         else if (world == 4)
         {
             uiController.timeRemaining = 150f;
             SetBrokenBottlesPosition(2);
 
-            oneStarCoins = 200;
-            twoStarCoins = 300;
-            threeStarCoins = 400;
+            oneStarCoins = 300;
+            twoStarCoins = 400;
+            threeStarCoins = 500;
         }
         else if (world == 5)
         {
             uiController.timeRemaining = 170f;
             SetBrokenBottlesPosition(2);
 
-            oneStarCoins = 300;
-            twoStarCoins = 400;
-            threeStarCoins = 500;
+            oneStarCoins = 400;
+            twoStarCoins = 500;
+            threeStarCoins = 600;
         }
         else if (world == 6)
         {
             uiController.timeRemaining = 170f;
             SetBrokenBottlesPosition(2);
 
-            oneStarCoins = 300;
-            twoStarCoins = 400;
-            threeStarCoins = 500;
+            oneStarCoins = 500;
+            twoStarCoins = 600;
+            threeStarCoins = 700;
         }
         else if (world == 7)
         {
             uiController.timeRemaining = 160f;
             SetBrokenBottlesPosition(3);
 
-            oneStarCoins = 300;
-            twoStarCoins = 400;
-            threeStarCoins = 500;
+            oneStarCoins = 600;
+            twoStarCoins = 700;
+            threeStarCoins = 800;
         }
         else if (world == 8)
         {
             uiController.timeRemaining = 180f;
             SetBrokenBottlesPosition(3);
 
-            oneStarCoins = 400;
-            twoStarCoins = 500;
-            threeStarCoins = 600;
+            oneStarCoins = 600;
+            twoStarCoins = 700;
+            threeStarCoins = 800;
         }
         else if (world == 9)
         {
             uiController.timeRemaining = 180f;
             SetBrokenBottlesPosition(3);
 
-            oneStarCoins = 400;
-            twoStarCoins = 500;
-            threeStarCoins = 600;
+            oneStarCoins = 700;
+            twoStarCoins = 800;
+            threeStarCoins = 900;
         }
         else if (world == 10)
         {
             uiController.timeRemaining = 170f;
             SetBrokenBottlesPosition(4);
 
-            oneStarCoins = 400;
-            twoStarCoins = 500;
-            threeStarCoins = 600;
+            oneStarCoins = 700;
+            twoStarCoins = 800;
+            threeStarCoins = 900;
         }
         else if (world == 11)
         {
             uiController.timeRemaining = 200f;
             SetBrokenBottlesPosition(4);
 
-            oneStarCoins = 500;
-            twoStarCoins = 600;
-            threeStarCoins = 700;
+            oneStarCoins = 800;
+            twoStarCoins = 900;
+            threeStarCoins = 1000;
         }
         else if (world == 12)
         {
             uiController.timeRemaining = 190f;
             SetBrokenBottlesPosition(4);
 
-            oneStarCoins = 500;
-            twoStarCoins = 600;
-            threeStarCoins = 700;
+            oneStarCoins = 800;
+            twoStarCoins = 900;
+            threeStarCoins = 1000;
         }
         else if (world == 13)
         {
             uiController.timeRemaining = 210f;
             SetBrokenBottlesPosition(4);
 
-            oneStarCoins = 600;
-            twoStarCoins = 700;
-            threeStarCoins = 800;
+            oneStarCoins = 2000;
+            twoStarCoins = 2100;
+            threeStarCoins = 2200;
         }
         else if (world == 14)
         {
             if (level == 1)
                 uiController.timeRemaining = 200f;
             else if (level == 2)
-                uiController.timeRemaining = 190f;
+                uiController.timeRemaining = 200f;
             else if (level == 3)
-                uiController.timeRemaining = 180f;
+                uiController.timeRemaining = 200f;
 
             SetBrokenBottlesPosition(4);
 
-            oneStarCoins = 600;
-            twoStarCoins = 700;
-            threeStarCoins = 800;
+            oneStarCoins = 2900;
+            twoStarCoins = 3000;
+            threeStarCoins = 3100;
         }
     }
 
