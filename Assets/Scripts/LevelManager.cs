21cf3aa5c3466996a6b693b3fc49229a936f9ca2
Augusto Polonio <augustodrp@gmail.com>
2020-11-01 18:17:01 -0300

    reopen #15 Level properties using JSON (fixed file);

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -13,7 +13,7 @@ public class LevelManager : MonoBehaviour
     public int twoStarCoins;
     public int threeStarCoins;
     public List<int> requestedColorsPosition;
-    public string path;
+    private string path;
 
     [System.Serializable]
     public class Level
@@ -26,14 +26,11 @@ public class LevelManager : MonoBehaviour
         public int threeStarCoins;
         public float time;
         public string color;
-        public List<int> orderColorsToBreak;
-
-        //Dynamic variables
-        // public int starsEarned;
-        // public int starsToUnlock;
+        public int starsToUnlock;
+        // public List<int> orderColorsToBreak;
+        public int qtyColorsToBreak;
     }
-
-    public List<Level> levelListGlobal;
+    private List<Level> levelListGlobal;
 
     [System.Serializable]
     public class LevelList
@@ -44,17 +41,18 @@ public class LevelManager : MonoBehaviour
     [System.Serializable]
     public class LevelProgress
     {
-        //Dynamic variables
+        public int world;
+        public int level;
         public int starsEarned;
-        public int starsToUnlock;
+        public int highscore;
     }
 
-    public List<Level> levelProgressListGlobal;
+    private List<LevelProgress> levelProgressListGlobal;
 
     [System.Serializable]
     public class LevelProgressList
     {
-        public List<Level> Levels;
+        public List<LevelProgress> LevelsProgress;
     }
 
 
@@ -63,29 +61,22 @@ public class LevelManager : MonoBehaviour
     {
         uiController = GameObject.FindGameObjectWithTag("UIController").GetComponent<UIController>();
 
-        world = int.Parse(SceneManager.GetActiveScene().name.Split('_')[0]);
-        level = int.Parse(SceneManager.GetActiveScene().name.Split('_')[1]);
-
         SetSelectedLevelVariables();
-
 
         path = Path.Combine(Application.dataPath, "LevelData.json");
 
         // levelListGlobal = new List<Level>();
 
-        //Code to generate JSON file
+        //UTIL: Code to generate JSON file
         //World        
-        for (int i = 1; i <= 14; i++)
-        {
-            //Level
-            for (int y = 1; y <= 3; y++)
-            {
-                // if (i < 5)
-                GetLevelProperties(i, y);
-            }
-        }
-
-        // LoadLevelDataJSON();
+        // for (int i = 1; i <= 14; i++)
+        // {
+        //     //Level
+        //     for (int y = 1; y <= 3; y++)
+        //     {
+        //         CreateLevelsJSON(i, y);
+        //     }
+        // }
     }
 
     public void Save(LevelList levelList)
@@ -107,48 +98,38 @@ public class LevelManager : MonoBehaviour
 
     private void SetSelectedLevelVariables()
     {
-        if (world == 1)
-        {
-            uiController.timeRemaining = 60f;
-            oneStarCoins = twoStarCoins = threeStarCoins = 100;
+        world = int.Parse(SceneManager.GetActiveScene().name.Split('_')[0]);
+        level = int.Parse(SceneManager.GetActiveScene().name.Split('_')[1]);
 
-            if (level == 1)
-            {
-                uiController.isTutorial = true;
-            }
+        string LevelDataJSON = File.ReadAllText(Application.dataPath + "/LevelData.json");
+        Level selectedLevel = GetLevelProperties(world, level, LevelDataJSON);//JsonUtility.FromJson<LevelList>(LevelDataJSON).Levels.Find(c => c.world == world && c.level == level);
+
+        oneStarCoins = selectedLevel.oneStarCoins;
+        twoStarCoins = selectedLevel.twoStarCoins;
+        threeStarCoins = selectedLevel.threeStarCoins;
+
+        uiController.timeRemaining = selectedLevel.time;
+        SetBrokenBottlesPosition(selectedLevel.qtyColorsToBreak);
+
+        //Conditions
+        if (world == 1 && level == 1)
+        {
+            uiController.isTutorial = true;
         }
         else if (world == 2)
         {
-            uiController.timeRemaining = 120f;
-
             if (level == 1)
             {
                 uiController.isToolTutorial = true;
                 requestedColorsPosition.Add(1);
             }
-            else
-            {
-                SetBrokenBottlesPosition(1);
-            }
-
-            oneStarCoins = 100;
-            twoStarCoins = 200;
-            threeStarCoins = 300;
-
-            if (level == 3 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 0)
+            else if (level == 3 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 0)
             {
                 uiController.isPowerUpTutorial = true;
             }
         }
         else if (world == 3)
         {
-            uiController.timeRemaining = 120f;
-            SetBrokenBottlesPosition(1);
-
-            oneStarCoins = 200;
-            twoStarCoins = 300;
-            threeStarCoins = 400;
-
             if (level == 2 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 1)
             {
                 uiController.isPowerUpTutorial = true;
@@ -156,13 +137,6 @@ public class LevelManager : MonoBehaviour
         }
         else if (world == 4)
         {
-            uiController.timeRemaining = 150f;
-            SetBrokenBottlesPosition(2);
-
-            oneStarCoins = 300;
-            twoStarCoins = 400;
-            threeStarCoins = 500;
-
             if (level == 1 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 2)
             {
                 uiController.isPowerUpTutorial = true;
@@ -174,13 +148,6 @@ public class LevelManager : MonoBehaviour
         }
         else if (world == 5)
         {
-            uiController.timeRemaining = 170f;
-            SetBrokenBottlesPosition(2);
-
-            oneStarCoins = 400;
-            twoStarCoins = 500;
-            threeStarCoins = 600;
-
             if (level == 2 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 4)
             {
                 uiController.isPowerUpTutorial = true;
@@ -188,237 +155,111 @@ public class LevelManager : MonoBehaviour
         }
         else if (world == 6)
         {
-            uiController.timeRemaining = 170f;
-            SetBrokenBottlesPosition(2);
-
-            oneStarCoins = 500;
-            twoStarCoins = 600;
-            threeStarCoins = 700;
-
             if (level == 1 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 5)
             {
                 uiController.isPowerUpTutorial = true;
             }
         }
-        else if (world == 7)
-        {
-            uiController.timeRemaining = 160f;
-            SetBrokenBottlesPosition(3);
-
-            oneStarCoins = 600;
-            twoStarCoins = 700;
-            threeStarCoins = 800;
-        }
-        else if (world == 8)
-        {
-            uiController.timeRemaining = 180f;
-            SetBrokenBottlesPosition(3);
-
-            oneStarCoins = 600;
-            twoStarCoins = 700;
-            threeStarCoins = 800;
-        }
-        else if (world == 9)
-        {
-            uiController.timeRemaining = 180f;
-            SetBrokenBottlesPosition(3);
-
-            oneStarCoins = 700;
-            twoStarCoins = 800;
-            threeStarCoins = 900;
-        }
-        else if (world == 10)
-        {
-            uiController.timeRemaining = 170f;
-            SetBrokenBottlesPosition(4);
-
-            oneStarCoins = 700;
-            twoStarCoins = 800;
-            threeStarCoins = 900;
-        }
-        else if (world == 11)
-        {
-            uiController.timeRemaining = 200f;
-            SetBrokenBottlesPosition(4);
-
-            oneStarCoins = 800;
-            twoStarCoins = 900;
-            threeStarCoins = 1000;
-        }
-        else if (world == 12)
-        {
-            uiController.timeRemaining = 190f;
-            SetBrokenBottlesPosition(4);
-
-            oneStarCoins = 800;
-            twoStarCoins = 900;
-            threeStarCoins = 1000;
-        }
-        else if (world == 13)
-        {
-            uiController.timeRemaining = 210f;
-            SetBrokenBottlesPosition(4);
-
-            oneStarCoins = 2000;
-            twoStarCoins = 2100;
-            threeStarCoins = 2200;
-        }
-        else if (world == 14)
-        {
-            if (level == 1)
-                uiController.timeRemaining = 200f;
-            else if (level == 2)
-                uiController.timeRemaining = 200f;
-            else if (level == 3)
-                uiController.timeRemaining = 200f;
-
-            SetBrokenBottlesPosition(4);
-
-            oneStarCoins = 2900;
-            twoStarCoins = 3000;
-            threeStarCoins = 3100;
-        }
     }
 
-    public void SetBrokenBottlesPosition(int howMany, Level level = null)
+    public Level GetLevelProperties(int world, int level, string LevelDataJSON)
     {
-        if (level != null)
+        // string LevelDataJSON = File.ReadAllText(Application.dataPath + "/LevelData.json");
+        Level levelProperties = JsonUtility.FromJson<LevelList>(LevelDataJSON).Levels.Find(c => c.world == world && c.level == level);
+
+        return levelProperties;
+    }
+
+    public void SetBrokenBottlesPosition(int howMany)
+    {
+        for (int i = 1; i <= howMany; i++)
         {
-            for (int i = 1; i <= howMany; i++)
-            {
-                if (i == 1)
-                    level.orderColorsToBreak.Add(Random.Range(1, 3)); //a < b  (1 to 2)
-                else if (i == 2)
-                    level.orderColorsToBreak.Add(Random.Range(3, 5)); //a < b  (3 to 4)
-                else if (i == 3)
-                    level.orderColorsToBreak.Add(Random.Range(5, 8)); //a < b  (5 to 7)
-                else if (i == 4)
-                    level.orderColorsToBreak.Add(Random.Range(8, 17)); //a < b  (8 to 16)
-            }
-        }
-        else
-        {
-            for (int i = 1; i <= howMany; i++)
-            {
-                if (i == 1)
-                    requestedColorsPosition.Add(Random.Range(1, 3)); //a < b  (1 to 2)
-                else if (i == 2)
-                    requestedColorsPosition.Add(Random.Range(3, 5)); //a < b  (3 to 4)
-                else if (i == 3)
-                    requestedColorsPosition.Add(Random.Range(5, 8)); //a < b  (5 to 7)
-                else if (i == 4)
-                    requestedColorsPosition.Add(Random.Range(8, 17)); //a < b  (8 to 16)
-            }
+            if (i == 1)
+                requestedColorsPosition.Add(Random.Range(1, 3)); //a < b  (1 to 2)
+            else if (i == 2)
+                requestedColorsPosition.Add(Random.Range(3, 5)); //a < b  (3 to 4)
+            else if (i == 3)
+                requestedColorsPosition.Add(Random.Range(5, 8)); //a < b  (5 to 7)
+            else if (i == 4)
+                requestedColorsPosition.Add(Random.Range(8, 17)); //a < b  (8 to 16)
         }
     }
+
+
+
+
+
 
     //Code to generate JSON file
-    public Level GetLevelProperties(int world, int level)
+    public Level CreateLevelsJSON(int world, int level)
     {
         Level levelProprierties = new Level();
+        LevelProgress levelProgress = new LevelProgress();
 
-        levelProprierties.world = world;
-        levelProprierties.level = level;
-
-        levelProprierties.orderColorsToBreak = new List<int>();
+        levelProprierties.world = levelProgress.world = world;
+        levelProprierties.level = levelProgress.level = level;
 
         if (world == 1)
         {
             levelProprierties.time = 60f;
             levelProprierties.oneStarCoins = levelProprierties.twoStarCoins = levelProprierties.threeStarCoins = 100;
 
-            if (level == 1)
-            {
-                uiController.isTutorial = true;
-            }
         }
         else if (world == 2)
         {
             levelProprierties.time = 120f;
 
-            if (level == 1)
-            {
-                uiController.isToolTutorial = true;
-                levelProprierties.orderColorsToBreak.Add(1);
-            }
-            else
-            {
-                SetBrokenBottlesPosition(1, levelProprierties);
-            }
+            levelProprierties.qtyColorsToBreak = 1;
 
             levelProprierties.oneStarCoins = 100;
             levelProprierties.twoStarCoins = 200;
             levelProprierties.threeStarCoins = 300;
-
-            if (level == 3 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 0)
-            {
-                uiController.isPowerUpTutorial = true;
-            }
         }
         else if (world == 3)
         {
             levelProprierties.time = 120f;
-            SetBrokenBottlesPosition(1, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 1;
 
             levelProprierties.oneStarCoins = 200;
             levelProprierties.twoStarCoins = 300;
             levelProprierties.threeStarCoins = 400;
-
-            if (level == 2 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 1)
-            {
-                uiController.isPowerUpTutorial = true;
-            }
         }
         else if (world == 4)
         {
             levelProprierties.time = 150f;
-            SetBrokenBottlesPosition(2, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 2;
 
             levelProprierties.oneStarCoins = 300;
             levelProprierties.twoStarCoins = 400;
             levelProprierties.threeStarCoins = 500;
-
-            if (level == 1 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 2)
-            {
-                uiController.isPowerUpTutorial = true;
-            }
-            else if (level == 3 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 3)
-            {
-                uiController.isPowerUpTutorial = true;
-            }
         }
         else if (world == 5)
         {
             levelProprierties.time = 170f;
-            SetBrokenBottlesPosition(2, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 2;
 
             levelProprierties.oneStarCoins = 400;
             levelProprierties.twoStarCoins = 500;
             levelProprierties.threeStarCoins = 600;
-
-            if (level == 2 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 4)
-            {
-                uiController.isPowerUpTutorial = true;
-            }
         }
         else if (world == 6)
         {
             levelProprierties.time = 170f;
-            SetBrokenBottlesPosition(2, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 2;
 
             levelProprierties.oneStarCoins = 500;
             levelProprierties.twoStarCoins = 600;
             levelProprierties.threeStarCoins = 700;
-
-            if (level == 1 && PlayerPrefs.GetInt("PowerUpsTutorial_Step") == 5)
-            {
-                uiController.isPowerUpTutorial = true;
-            }
         }
         else if (world == 7)
         {
             levelProprierties.time = 160f;
-            SetBrokenBottlesPosition(3, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 3;
 
             levelProprierties.oneStarCoins = 600;
             levelProprierties.twoStarCoins = 700;
@@ -427,7 +268,8 @@ public class LevelManager : MonoBehaviour
         else if (world == 8)
         {
             levelProprierties.time = 180f;
-            SetBrokenBottlesPosition(3, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 3;
 
             levelProprierties.oneStarCoins = 600;
             levelProprierties.twoStarCoins = 700;
@@ -436,7 +278,8 @@ public class LevelManager : MonoBehaviour
         else if (world == 9)
         {
             levelProprierties.time = 180f;
-            SetBrokenBottlesPosition(3, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 3;
 
             levelProprierties.oneStarCoins = 700;
             levelProprierties.twoStarCoins = 800;
@@ -445,7 +288,8 @@ public class LevelManager : MonoBehaviour
         else if (world == 10)
         {
             levelProprierties.time = 170f;
-            SetBrokenBottlesPosition(4, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 4;
 
             levelProprierties.oneStarCoins = 700;
             levelProprierties.twoStarCoins = 800;
@@ -454,7 +298,8 @@ public class LevelManager : MonoBehaviour
         else if (world == 11)
         {
             levelProprierties.time = 200f;
-            SetBrokenBottlesPosition(4, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 4;
 
             levelProprierties.oneStarCoins = 800;
             levelProprierties.twoStarCoins = 900;
@@ -463,7 +308,8 @@ public class LevelManager : MonoBehaviour
         else if (world == 12)
         {
             levelProprierties.time = 190f;
-            SetBrokenBottlesPosition(4, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 4;
 
             levelProprierties.oneStarCoins = 800;
             levelProprierties.twoStarCoins = 900;
@@ -472,7 +318,8 @@ public class LevelManager : MonoBehaviour
         else if (world == 13)
         {
             levelProprierties.time = 210f;
-            SetBrokenBottlesPosition(4, levelProprierties);
+
+            levelProprierties.qtyColorsToBreak = 4;
 
             levelProprierties.oneStarCoins = 2000;
             levelProprierties.twoStarCoins = 2100;
@@ -487,22 +334,15 @@ public class LevelManager : MonoBehaviour
             else if (level == 3)
                 levelProprierties.time = 200f;
 
-            SetBrokenBottlesPosition(4, levelProprierties);
+            levelProprierties.qtyColorsToBreak = 4;
 
             levelProprierties.oneStarCoins = 2900;
             levelProprierties.twoStarCoins = 3000;
             levelProprierties.threeStarCoins = 3100;
         }
 
-        // PlayerData playerData = new PlayerData();
-        // playerData.teste1 = 111;
-        // playerData.teste1 = 222;
-
-        // Save(levelProprierties);
-        // print("json: " + JsonUtility.ToJson(playerData));
-        // print("json: " + JsonUtility.ToJson(levelProprierties));
-
         levelListGlobal.Add(levelProprierties);
+        levelProgressListGlobal.Add(levelProgress);
 
         if (world == 14 && level == 3)
         {
@@ -510,8 +350,37 @@ public class LevelManager : MonoBehaviour
             ql.Levels = levelListGlobal;
             // print("json: " + JsonUtility.ToJson(ql));
             Save(ql);
+
+
+            //Level progress JSON
+            var ql2 = new LevelProgressList();
+            ql2.LevelsProgress = levelProgressListGlobal;
+            // print("json: " + JsonUtility.ToJson(ql2));
+
+            PlayerPrefs.SetString("_LevelProgress", JsonUtility.ToJson(ql2));
         }
 
         return levelProprierties;
+    }
+
+    void Update()
+    {
+
+        //Teste to set highscore of world 2 level 1 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //TODO: create a global string json variable to load on Start()????
+            //OR NOT?? (or load every time the we need to set data)????
+            string levelProgressJSON = PlayerPrefs.GetString("_LevelProgress");
+            //Get world 2 level 1 data
+            LevelProgressList levelsProgress = JsonUtility.FromJson<LevelProgressList>(levelProgressJSON);
+            LevelProgress level = levelsProgress.LevelsProgress.Find(c => c.world == 2 && c.level == 1);
+
+            //Set world 2 level 1 data
+            level.highscore = 350;
+            PlayerPrefs.SetString("_LevelProgress", JsonUtility.ToJson(levelsProgress));
+
+            print(JsonUtility.ToJson(levelsProgress));
+        }
     }
 }
