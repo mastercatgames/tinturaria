00e640fe65fbbc0bbf813ff1cc548c2f23cff866
Augusto Polonio <augustodrp@gmail.com>
2021-01-27 19:39:46 -0300

    reopen #29 Settings and Credits screen was finished (but without icons yet)

---
+++ Assets/Scripts/SocialMedia.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialMedia : MonoBehaviour
+{
+    public void OpenInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/mastercatgamestudio/");
+    }
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/mastercatgames/");
+    }
+
+    public void OpenFacebook()
+    {
+        Application.OpenURL("https://www.facebook.com/mastercatgamestudio/");
+    }
+
+    public void OpenCustomURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
